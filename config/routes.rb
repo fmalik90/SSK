e316f776290c44771902e5eb87bc0ab45992057c
Faryal Malik faryal.abid@gmail.com <faryal.abid@gmail.com>
2023-02-08 01:53:13 +0000

    testing out devise

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :flights, only: [:index, :show] do
     resources :bookings, only: [:show, :new, :create]
   end
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
