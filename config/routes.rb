175ef3dd9ee44d23eb297c9a3ff963ddcc9641de
Ellen CHan <ellenycc@gmail.com>
2023-02-11 12:55:57 +0000

    Fix conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'flights#index'
   resources :flights, only: [:index, :show] do
     resources :bookings, only: [:show, :new, :create]
   end
