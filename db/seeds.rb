4385d14dc5ce0b0eb6d39b0b8f297ac304f1a41b
Ellen CHan <ellenycc@gmail.com>
2023-02-16 15:35:37 +0000

    Reset seeds, use form to add flights with images and edit style

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,66 +13,66 @@ puts "Database cleaned"
 
     Flight.create([
     {
-      destination: "Mercury",
+      destination: "Test",
       date: "10/10/2028",
       time: "10:35",
       description: "The closest planet to the sun",
       duration: 24,
       price: 3999
-    },
-    {
-      destination: "Venus",
-      date: "28/02/2032",
-      time: "14:05",
-      description: "Earth's solar system twin",
-      duration: 22,
-      price: 5999
-    },
-    {
-      destination: "Mars",
-      date: "13/05/2046",
-      time: "12:34",
-      description: "The red planet",
-      duration: 18,
-      price: 6999
-    },
-    {
-      destination: "Jupiter",
-      date: "25/12/2036",
-      time: "19:15",
-      description: "The largest planet in the solar system",
-      duration: 24,
-      price: 4999
-    },
-    {
-      destination: "Saturn",
-      date: "19/7/2042",
-      time: "13:00",
-      description: "The Ringed Jewel",
-      duration: 24,
-      price: 5000
-    },
-    {
-      destination: "Uranus",
-      date: "31/12/2031",
-      time: "16:45",
-      description: "The tilted, sideways planet",
-      duration: 24,
-      price: 7999
-    },
-    {
-      destination: "Neptune",
-      date: "1/5/2043",
-      time: "11:30",
-      description: "A giant, stormy blue planet",
-      duration: 26,
-      price: 5999
-    },
-    {
-      destination: "Pluto",
-      date: "10/10/2027",
-      time: "23:45",
-      description: "Once a planet, now a dwarf planet",
-      duration: 34,
-      price: 6999
     }])
+    # {
+    #   destination: "Venus",
+    #   date: "28/02/2032",
+    #   time: "14:05",
+    #   description: "Earth's solar system twin",
+    #   duration: 22,
+    #   price: 5999
+    # },
+    # {
+    #   destination: "Mars",
+    #   date: "13/05/2046",
+    #   time: "12:34",
+    #   description: "The red planet",
+    #   duration: 18,
+    #   price: 6999
+    # },
+    # {
+    #   destination: "Jupiter",
+    #   date: "25/12/2036",
+    #   time: "19:15",
+    #   description: "The largest planet in the solar system",
+    #   duration: 24,
+    #   price: 4999
+    # },
+    # {
+    #   destination: "Saturn",
+    #   date: "19/7/2042",
+    #   time: "13:00",
+    #   description: "The Ringed Jewel",
+    #   duration: 24,
+    #   price: 5000
+    # },
+    # {
+    #   destination: "Uranus",
+    #   date: "31/12/2031",
+    #   time: "16:45",
+    #   description: "The tilted, sideways planet",
+    #   duration: 24,
+    #   price: 7999
+    # },
+    # {
+    #   destination: "Neptune",
+    #   date: "1/5/2043",
+    #   time: "11:30",
+    #   description: "A giant, stormy blue planet",
+    #   duration: 26,
+    #   price: 5999
+    # },
+    # {
+    #   destination: "Pluto",
+    #   date: "10/10/2027",
+    #   time: "23:45",
+    #   description: "Once a planet, now a dwarf planet",
+    #   duration: 34,
+    #   price: 6999
+    # }])
